a3b36bce17ec6e97b2187afc82b14d2271e87a36
Patrick Magee <patrick.magee@geeks.ltd.uk>
2015-04-20 12:23:40 +0100

    Namespace cleanup.

--- Formatter.Tests/Properties/AssemblyInfo.cs
+++ Formatter.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MSharp.Code.Formatter.Tests")]
+[assembly: AssemblyTitle("Formatter.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MSharp.Code.Formatter.Tests")]
+[assembly: AssemblyProduct("Formatter.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
